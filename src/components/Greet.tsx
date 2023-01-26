591a03aa83830f7566ea82eea4d7615dbfd21f04
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-01-26 14:52:31 +0100

    type of data used in typescript

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,10 +1,20 @@
 type GreetProps = {
-    name : string,
-    messageCount : number,
+  name: string;
+  messageCount?: number;
+  isLoggedIn: boolean;
 };
 
 export const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props;
   return (
-    <div>Welcome {props.name}! You have unread messages</div>
-  )
-}
+    <div>
+      {props.isLoggedIn ? (
+        <div>
+          Welcome {props.name}! You have {messageCount} unread messages
+        </div>
+      ) : (
+        <div>Welcome Guest</div>
+      )}
+    </div>
+  );
+};
